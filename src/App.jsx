92d3efda4d7c23eb79d2a854a34afea4e62bce1f
Nicholas Chai <nicholas.chai@outlook.com>
2024-02-04 16:57:25 +0800

    added some pages as additional scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layouts";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "/", element: <div>About</div> },
+      { path: "/", element: <Home /> },
+      { path: "/users", element: <Users /> },
     ]
   },
 ]);
